2f3f866be688646b5bd0c07d8654d0fd854eb2d6
Gamma-user012 <gamma-user012@greendreamteam.onmicrosoft.com>
2025-11-03 21:13:42 +0200

    Added line collision checks to Box

--- server/Level.py
+++ server/Level.py
@@ -18,21 +18,53 @@ class Box:
         return (self.x1 <= x <= self.x2) and (self.y1 <= y <= self.y2)
 
     def check_collision_line(self, x1: int, y1: int, x2: int, y2: int):
-        ...
+        c0, c1, c2, c3 = self._get_corners()
+        return any([
+            self._check_collision_line_horizontal(x1, y1, x2, y2, c0[0], c1[0], c0[1]),
+            self._check_collision_line_horizontal(x1, y1, x2, y2, c3[0], c2[0], c2[1]),
+            self._check_collision_line_vertical(x1, y1, x2, y2, c1[1], c2[1], c1[0]),
+            self._check_collision_line_vertical(x1, y1, x2, y2, c0[1], c3[1], c0[0])
+        ])
 
     def _get_corners(self):
-        ...
-        # return [(self.x1, self.y1), (self.x2, self.y1), (self.)]
+        return [(self.x1, self.y1), (self.x2, self.y1), (self.x2, self.y2), (self.x1, self.y2)]
+        # 3 2
+        # 0 1
 
     @staticmethod
     def _check_collision_line_line(x1: int, y1: int, x2: int, y2: int, x3: int, y3: int, x4: int, y4: int):
         t = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
         r = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
-        return (0 <= t <= 1) and (0 <= r <= 1)
+        if (0 <= t <= 1) and (0 <= r <= 1):
+            return (x1 + t * (x2 - x1), y1 + t * (y2 - y1))
+        else:
+            return False
 
     @staticmethod
-    def _check_collision_line_horizontal(x1: int, y1: int, x2: int, y2: int):
-        ...
+    def _check_collision_line_horizontal(x1: int, y1: int, x2: int, y2: int, x3: int, x4: int, y3: int):
+        if y1 > y2:
+            y1, y2 = y2, y1
+            x1, x2 = x2, x1
+        if (y3 < y1) or (y3 > y2):
+            return False
+        t = (y3 - y1) / (y2 - y1)
+        tx = x1 + t * (x2 - x1)
+        if (x3 <= tx <= x4) or (x4 <= tx <= x3):
+            return (tx, y3)
+        return False
+
+    @staticmethod
+    def _check_collision_line_vertical(x1: int, y1: int, x2: int, y2: int, y3: int, y4: int, x3: int):
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+        if (x3 < x1) or (x3 > x2):
+            return False
+        t = (x3 - x1) / (x2 - x1)
+        ty = y1 + t * (y2 - y1)
+        if (y3 <= ty <= y4) or (y4 <= ty <= y3):
+            return (x3, ty)
+        return False
 
 
 class Level:
