6f83b51e85c2aaddeccc82c29f109e3712a2654d
Gamma-user012 <gamma-user012@greendreamteam.onmicrosoft.com>
2025-11-03 20:29:16 +0200

    Added Player class for saving player values

--- server/Player.py
+++ server/Player.py
@@ -1,2 +1,28 @@
 class Player:
-    ...
+
+    x: int
+    y: int
+    hp: int
+    team: int
+
+
+    MIN_HP = 0
+    MAX_HP = 100
+    BOMB_DAMAGE = 50
+
+    def __init__(self, team: int):
+        self.x = 0
+        self.y = 0
+        self.hp = self.MAX_HP
+        self.team = team
+
+    def make_hit(self):
+        self.hp -= self.BOMB_DAMAGE
+        self.check_death()
+
+    def check_death(self):
+        if self.hp < self.MIN_HP:
+            ...
+
+    def same_team(self, team):
+        return self.team == team
