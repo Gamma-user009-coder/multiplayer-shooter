807f99606537d5306bb970a253a094181d0f61a3
Gamma-user012 <gamma-user012@greendreamteam.onmicrosoft.com>
2025-11-11 20:26:21 +0200

    Fixed division by zero error in Level

--- server/Level.py
+++ server/Level.py
@@ -57,6 +57,13 @@ class Box:
         if y1 > y2:
             y1, y2 = y2, y1
             x1, x2 = x2, x1
+        if y1 == y2:
+            if y1 == y3:
+                if x1 >= x3 >= x2 or x2 >= x3 >= x1:
+                    return Collision(x3, y3)
+                elif x1 >= x4 >= x2 or x2 >= x4 >= x1:
+                    return Collision(x4, y3)
+            return Collision.false()
         if (y3 < y1) or (y3 > y2):
             return Collision.false()
         t = (y3 - y1) / (y2 - y1)
@@ -71,6 +78,13 @@ class Box:
         if x1 > x2:
             x1, x2 = x2, x1
             y1, y2 = y2, y1
+        if x1 == x2:
+            if x1 == x3:
+                if y1 >= y3 >= y2 or y2 >= y3 >= y1:
+                    return Collision(x3, y3)
+                elif y1 >= y4 >= y2 or y2 >= y4 >= y1:
+                    return Collision(x3, y4)
+            return Collision.false()
         if (x3 < x1) or (x3 > x2):
             return Collision.false()
         t = (x3 - x1) / (x2 - x1)
