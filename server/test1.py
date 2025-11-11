495206a35517466d967d370fea5fe12da9a52fda
Itay Hadas <gamma-user005@greendreamteam.onmicrosoft.com>
2025-11-11 21:55:26 +0200

    feat: change the test to send first connection packet and print the server response

--- server/test1.py
+++ server/test1.py
@@ -5,10 +5,11 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     sock.connect(('127.0.0.1', 12345))
-    dct = {"id": 2}
+    dct = {"id": 3}
     sock.send(json.dumps(dct).encode())
     print("sent")
-    sock.recvfrom(1024)
+    print(sock.recvfrom(1024)[0].decode())
+    sock.close()
 
 if __name__ == '__main__':
     main()
